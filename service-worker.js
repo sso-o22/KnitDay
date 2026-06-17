4a6f201a17b8924230c690c33af66e79c962b997
sso-o22 <sso-o22@users.noreply.github.com>
2026-06-17 02:12:09 +0000

    deploy: 5b90007dd61829dac0e3a88b91f4d28e456108e0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: xfsn7aEu */
+/* Manifest version: PWIOjv+K */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
