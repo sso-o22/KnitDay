a2d0ee6ce03f605056775411fde24920e2008ce0
sso-o22 <sso-o22@users.noreply.github.com>
2026-06-18 04:00:07 +0000

    deploy: f638f9944dcce05af99dff3ec1c64eb1adf38b47

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "e0vRAT+B",
+  "version": "3osuqmtv",
   "assets": [
     {
       "hash": "sha256-OEk/nNAi+lNWDSdgVJwDvTQrWaeYfQWlQYdE+m/8wG8=",
@@ -10,7 +10,7 @@ self.assetsManifest = {
       "url": "BlazorApp1.styles.css"
     },
     {
-      "hash": "sha256-sXrOgRD9RAfKErT0hSKVNTGj2nMlBHNeT/Oc2xHi8No=",
+      "hash": "sha256-faodwwnWV385zi/VHk9KByqr6J1OkwFxtO+nk5EgIek=",
       "url": "_framework/BlazorApp1.wasm"
     },
     {
@@ -166,7 +166,7 @@ self.assetsManifest = {
       "url": "_framework/System.wasm"
     },
     {
-      "hash": "sha256-OzTJdObcxhz4ePfEZ/97B7CTOd4YuvrBwIrLc8pPGQA=",
+      "hash": "sha256-HwsN84CEgu4IuItml754wyf79+D7QsAuWscVjDZdcQE=",
       "url": "_framework/blazor.boot.json"
     },
     {
@@ -2534,7 +2534,7 @@ self.assetsManifest = {
       "url": "sample-data/weather.json"
     },
     {
-      "hash": "sha256-s+vdLLN53lF3wbiip7grLEL1Jo01S72GIxSutAjhP5s=",
+      "hash": "sha256-RIqse9Rb3PRDdg/pPgAgaZMV/2DAaZ4YCexVTPvuKas=",
       "url": "version.json"
     }
   ]
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: e0vRAT+B */
+/* Manifest version: 3osuqmtv */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
